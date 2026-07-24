055c72ae1ce7ed1a3d2a3ed48271c8ce383581cc
linliji <dev@linliji.local>
2026-07-24 23:40:01 +0800

    前端API统一指向线上后端 https://linliji.onrender.com (web+小程序)

--- miniprogram/config.js
+++ miniprogram/config.js
@@ -3,5 +3,5 @@
 // 上线阶段：改成你的公网 HTTPS 地址（必须有 SSL），并在微信公众平台 -> 开发管理 -> 服务器域名 中
 // 把该域名加入 request 合法域名（以及 uploadFile/downloadFile 如用到图片）。
 module.exports = {
-  API_BASE: 'http://127.0.0.1:3399'
+  API_BASE: 'https://linliji.onrender.com'
 };
